b120488248b4cd400c2f2365fffd6a036c0a7f82
Jay Vigilla <JayRVigilla@gmail.com>
2020-04-29 12:36:52 -0700

    jay went rogue. we forgot to change the URL in the CompanyCard to 'companies/handle'. Now works with handle across the board

--- jobly-frontend/src/CompanyCard.js
+++ jobly-frontend/src/CompanyCard.js
@@ -7,10 +7,10 @@ function CompanyCard({ handle, name, description, logoUrl }) {
   //TBD on click of div, render filered job list for this company
   // keep company name and description at top of job list
   // TODO: make sure logos render, just showing alt 4/29
-  
+
   return (
     <li key={handle}>
-      <a href={`companies/${name}`}>
+      <a href={`companies/${handle}`}>
       <h5>{name}</h5>
       <p>{description}</p>
       <img src={logoUrl} alt={`${name} logo`} />
